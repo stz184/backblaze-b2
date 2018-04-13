a67bd8e59a5a57a820aeb13179b570150abeb373
teleport-palis <cpalis@teleport.earth>
2018-04-13 15:11:19 +0800

    updated authorize url

--- conf.js
+++ conf.js
@@ -1,5 +1,5 @@
 module.exports = {
-    API_AUTHORIZE__URL: 'https://api.backblaze.com/b2api/v1/b2_authorize_account',
+    API_AUTHORIZE__URL: 'https://api.backblazeb2.com/b2api/v1/b2_authorize_account',
     API_VERSION_URL: '/b2api/v1',
     MAX_INFO_HEADERS: 10    // maximum number of custom x-bz-info-* headers
 };
